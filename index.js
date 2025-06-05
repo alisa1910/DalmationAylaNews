16280750863e208658b58937bc9e9507b14fa7ae
alisa1910 <alisaafonina19@gmail.com>
2025-06-05 16:06:56 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+function openPopup() {
+    var popup = document.querySelector('.popup');
+    popup.style.display = 'block';
+  }
+  function closePopup() {
+    var popup = document.querySelector('.popup');
+    popup.style.display = 'none';
+  }
+  
+  
