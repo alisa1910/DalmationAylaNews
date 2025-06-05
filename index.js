f72c18766db5c25df657f2d3e033051b1dfeefa9
alisa1910 <alisaafonina19@gmail.com>
2025-06-05 16:43:07 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,1 @@
-function openPopup() {
-    var popup = document.querySelector('.popup');
-    popup.style.display = 'block';
-  }
-  function closePopup() {
-    var popup = document.querySelector('.popup');
-    popup.style.display = 'none';
-  }
-  
-  
+
